d3cc2bf1db75cbecd4e591bc5e9d280376e2808c
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2024-10-04 16:45:20 +0530

    login/signup page made

--- dhanvantri/src/components/Account.jsx
+++ dhanvantri/src/components/Account.jsx
@@ -1,16 +1,48 @@
-import { div } from 'framer-motion/client'
-import React from 'react'
+import React from 'react';
+import doctor from '../assets/doctorBG.jpg';
+import patient from '../assets/patientBG.png';
 
 const Account = () => {
   return (
-    // <div className="w-full lg:mt-[6.7rem] py-3 text-6xl font-bold flex justify-center text-center ">
-    //   <h1 class="text-6xl py-2 font-extrabold gradient-text">Sign Up As</h1>
-    // </div>
-    <div className='flex justify-between h-[1000px] lg:mt-[6.7rem]'>
-      <div className='w-full h-full border-r border-n-6'>a</div>
-      <div className='w-full'>b</div>
+    <div className='flex fixed justify-between h-[1000px] lg:mt-[6.7rem]'>
+      <div
+        style={{
+          backgroundImage: `url(${doctor})`,
+          backgroundSize: 'cover',
+          height: '100%',
+          backgroundPosition: 'center', // Center the image
+        }}
+        className='w-[1365px] relative' // Add relative positioning for absolute children
+      >
+        <div className='bg-black opacity-50 h-full hover:bg-black hover:opacity-70 transition duration-300'>
+          <div className='absolute inset-0 flex pt-48 justify-center opacity-0 hover:opacity-100 transition duration-300'>
+            <div className='text-white text-7xl font-calibri font-extrabold'>
+              <div>Sign up as</div>
+              <div>Doctor</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${patient})`,
+          backgroundSize: 'cover',
+          height: '100%',
+          backgroundPosition: 'center', // Center the image
+        }}
+        className='w-full relative' // Add relative positioning for absolute children
+      >
+        <div className='bg-black opacity-50 h-full hover:bg-black hover:opacity-70 transition duration-300'>
+          <div className='absolute inset-0 flex pt-48 justify-center opacity-0 hover:opacity-100 transition duration-300'>
+            <div className='text-white text-7xl font-calibri font-extrabold'>
+              <div>Sign up as</div>
+              <div>Patient</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
