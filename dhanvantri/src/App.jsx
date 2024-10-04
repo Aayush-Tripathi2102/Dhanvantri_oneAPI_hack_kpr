b2cc0d97d87f8e4bad45e79b0f9438ad67f68097
ai977 <rjunmenon200513@gmail.com>
2024-10-04 17:28:24 +0530

    Dataset Updated

---
+++ dhanvantri/src/App.jsx
@@ -0,0 +1,96 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Hospitals from "./components/Hospitals";
+import Login from "./components/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Pharmacy from "./components/Pharmacy";
+import Emergency from "./components/Emergency";
+import Ved from "./components/Ved";
+import Account from "./components/Account";
+import Specialists from "./components/Specialists";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <>
+          <Header />
+          <Login />
+        </>
+      ),
+    },
+    {
+      path: "/hospitals",
+      element: (
+        <>
+          <Header />
+          <Hospitals />
+        </>
+      ),
+    },
+    {
+      path: "/emergency",
+      element: (
+        <>
+          <Header />
+          <Emergency />
+        </>
+      ),
+    },
+    {
+      path: "/pharmacy",
+      element: (
+        <>
+          <Header />
+          <Pharmacy />
+        </>
+      ),
+    },
+    {
+      path: "/ved",
+      element: (
+        <>
+          <Header />
+          <Ved />
+        </>
+      ),
+    },
+    {
+      path: "/signup",
+      element: (
+        <>
+          <Header />
+          <Account />
+        </>
+      ),
+    },
+    {
+      path: "/specialists",
+      element: (
+        <>
+          <Header />
+          <Specialists />
+        </>
+      ),
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router}>
+      <Header />
+      <Home />
+    </RouterProvider>
+  );
+}
+
+export default App;
