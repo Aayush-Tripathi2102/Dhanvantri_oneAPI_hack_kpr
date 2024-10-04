de642a5f8fbaa72db5ca71551d98ca02bc79ddd7
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2024-10-04 13:14:00 +0530

    Initial Commit

---
+++ dhanvantri/src/components/Account.jsx
@@ -0,0 +1,16 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+const Account = () => {
+  return (
+    // <div className="w-full lg:mt-[6.7rem] py-3 text-6xl font-bold flex justify-center text-center ">
+    //   <h1 class="text-6xl py-2 font-extrabold gradient-text">Sign Up As</h1>
+    // </div>
+    <div className='flex justify-between h-[1000px] lg:mt-[6.7rem]'>
+      <div className='w-full h-full border-r border-n-6'>a</div>
+      <div className='w-full'>b</div>
+    </div>
+  )
+}
+
+export default Account
---
+++ dhanvantri/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="w-full lg:mt-[6.7rem] py-3 text-6xl font-bold flex justify-center text-center ">
+      <h1 class="text-7xl py-2 font-extrabold gradient-text">LOGIN</h1>
+    </div>
+  );
+};
+
+export default Login;
